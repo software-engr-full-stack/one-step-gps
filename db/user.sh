ce06a0b0f4e8eacc5b9e180dd2be690f440d617b
software-engr-full-stack <software.engr.full.stack@gmail.com>
2023-02-19 22:04:03 -0800

    Change user script from just creating user to resetting user

--- db/user.sh
+++ db/user.sh
@@ -4,6 +4,7 @@ run() {
   local pw="${1:?ERROR => must pass pw}"
 
   sudo mysql <<EOF
+DROP USER 'go'@'localhost';
 CREATE USER 'go'@'localhost';
 GRANT SELECT, INSERT ON one_step_gps.* TO 'go'@'localhost';
 
